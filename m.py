c89e88f57392642a7a517e01d46ca7699737bbc6
FareehaAfsheen <fareehaafsheenchowdhury@gmail.com>
2026-09-18 16:36:02 +0600

    user input

--- m.py
+++ m.py
@@ -41,3 +41,82 @@ while True:
     print("6. Transfer Money")
     print("7. Delete Account")
     print("8. Exit")
+
+    choice = input('Enter your choice: ')
+
+    if choice == '1':
+        name = input("Account holder name: ")
+        balance = float(input("Initial Balance: "))
+
+        new_acc = BankAccount(name, balance)
+        accounts.append(new_acc)
+
+        print(f"Account created successfully for {name}")
+        print("Balance:", new_acc.get_balance())
+
+    elif choice == '2':
+        if len(accounts) == 0:
+            print("No accounts found")
+        else:
+            for acc in accounts:
+                print("\n --Account Info--")
+                acc.show()
+
+    elif choice == '3':
+        name = input("Enter account holder name: ")
+        acc = find_account(accounts, name)
+
+        if acc is None:
+            print("Account not found")
+        else:
+            print("Balance:", acc.get_balance())
+
+    elif choice == '4':
+        name = input("Enter account holder name: ")
+        acc = find_account(accounts, name)
+
+        if acc is None:
+            print("Account not found")
+        else:
+            deposit_amount = float(input("Enter deposit amount: "))
+            acc.deposit(deposit_amount)
+            print("Final Balance:", acc.get_balance())
+
+    elif choice == '5':
+        name = input("Enter account holder name: ")
+        acc = find_account(accounts, name)
+
+        if acc is None:
+            print("Account not found")
+        else:
+            withdraw_amount = float(input("Enter Withdraw Amount: "))
+            acc.withdraw(withdraw_amount)
+            print("Final Balance:", acc.get_balance())
+
+    elif choice == '6':
+        sender_name = input("Enter sender account name: ")
+        sender = find_account(accounts, sender_name)
+
+        if sender is None:
+            print("Sender account not found")
+
+        else:
+            receiver_name = input("Enter receiver account name: ")
+            receiver = find_account(accounts, receiver_name)
+
+            if receiver is None:
+                print("Receiver account not found")
+
+            else:
+                amount = float(input("Enter transfer amount: "))
+
+                if amount > sender.get_balance():
+                    print("Insufficient Balance")
+
+                else:
+                    sender.withdraw(amount)
+                    receiver.deposit(amount)
+
+                    print("Transfer successful!")
+                    print("Sender Balance:", sender.get_balance())
+                    print("Receiver Balance:", receiver.get_balance())
